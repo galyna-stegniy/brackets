72f7fe289cf7975b6ff653c9b12db083a1ca7d80
Galyna Stegniy <galyna.stegniy@gmail.com>
2019-10-14 21:03:41 +0300

    solution: remove pairs from string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let pairs = [];
+    for (let c = 0; c < bracketsConfig.length; c++) {
+        pairs.push(bracketsConfig[c].join(''));
+    }
+    for (let i = str.length; i >= 0; i--) {
+        for (let j = 0; j < pairs.length; j++) {
+            if (str.includes(pairs[j])) {
+                str = str.replace(pairs[j], '');
+            }
+        }
+    }
+    return str === '';
+};
